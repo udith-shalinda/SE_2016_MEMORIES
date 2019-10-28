477ca063cfb09695a3da380c7abf804a894ff762
Udith <udithshalinda2@gmail.com>
2019-10-28 23:37:21 +0530

    react-photo-gallery added

--- src/app/components/HomePage/HomePage.jsx
+++ src/app/components/HomePage/HomePage.jsx
@@ -3,8 +3,53 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {Image} from 'semantic-ui-react'
 import { logOutUser } from '../../../actions/auth/authActions';
+import Gallery from 'react-photo-gallery';
 
 const HomePage = ({user, logOutUser}) => {
+
+  const PHOTO_SET = [
+    {
+      src: 'https://avatars2.githubusercontent.com/u/37300841?s=460&v=4',
+      width: 2,
+      height: 3
+    },
+    {
+      src: 'https://avatars3.githubusercontent.com/u/37531139?s=460&v=4',
+      width: 2,
+      height: 1
+    },
+    {
+      src: 'https://avatars0.githubusercontent.com/u/37216970?s=460&v=4',
+      width: 1,
+      height: 3
+    },
+    {
+      src: 'https://avatars0.githubusercontent.com/u/23637279?s=460&v=4',
+      width: 3,
+      height: 1
+    },
+    {
+      src: 'http://example.com/example/img1.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'http://example.com/example/img2.jpg',
+      width: 1,
+      height: 1
+    },
+    {
+      src: 'http://example.com/example/img1.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'http://example.com/example/img2.jpg',
+      width: 1,
+      height: 1
+    }
+  ];
+
   return (
     <div>
       Home
@@ -18,6 +63,10 @@ const HomePage = ({user, logOutUser}) => {
         <p>{user.username}</p>
         <p>{user.email}</p>
       </div>}
+
+      <div>    
+        <Gallery photos={PHOTO_SET} />
+      </div>
     </div>
   );
 }
